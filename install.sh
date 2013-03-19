c32d9bbc4ed5ddc0827f07c94be5e4fd0ba36f80
Thomas Meyssonnier <tmeysson@gmail.com>
2013-03-19 19:38:13 +0100

    install.sh for dylib: first version

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
 sudo cp libIscore.a /usr/local/lib/
-sudo cp libIscore.so.1.0.0 /usr/local/lib/
-sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so.1.0
-sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so.1
-sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so
+if [ -x libIscore.so.1.0.0 ]
+then
+	sudo cp libIscore.so.1.0.0 /usr/local/lib/
+	sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so.1.0
+	sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so.1
+	sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so
+fi
+if [ -x libIscore.dylib.1.0.0 ]
+then
+	sudo cp libIscore.dylib.1.0.0 /usr/local/lib/
+	sudo ln -s /usr/local/lib/libIscore.dylib.1.0.0 /usr/local/lib/libIscore.dylib.1.0
+	sudo ln -s /usr/local/lib/libIscore.dylib.1.0.0 /usr/local/lib/libIscore.dylib.1
+	sudo ln -s /usr/local/lib/libIscore.dylib.1.0.0 /usr/local/lib/libIscore.dylib
+fi
 sudo ldconfig
 sudo mkdir -p /usr/local/include/IScore
 sudo cp -R Includes/* /usr/local/include/IScore/
+
