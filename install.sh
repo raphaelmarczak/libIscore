5236429992395f17a8bbd9fb4d0a3742f995b4b0
Thomas Meyssonnier <tmeysson@gmail.com>
2013-03-22 17:25:21 +0100

    Fixed MacOS compilation

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-sudo cp libIscore.a /usr/local/lib/
 if [ -x libIscore.so.1.0.0 ]
 then
+	sudo cp libIscore.a /usr/local/lib/
 	sudo cp libIscore.so.1.0.0 /usr/local/lib/
 	sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so.1.0
 	sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so.1
 	sudo ln -s /usr/local/lib/libIscore.so.1.0.0 /usr/local/lib/libIscore.so
+	sudo ldconfig
 fi
-if [ -x libIscore.dylib.1.0.0 ]
+if [ -x libIscore.1.0.0.dylib ]
 then
-	sudo cp libIscore.dylib.1.0.0 /usr/local/lib/
-	sudo ln -s /usr/local/lib/libIscore.dylib.1.0.0 /usr/local/lib/libIscore.dylib.1.0
-	sudo ln -s /usr/local/lib/libIscore.dylib.1.0.0 /usr/local/lib/libIscore.dylib.1
-	sudo ln -s /usr/local/lib/libIscore.dylib.1.0.0 /usr/local/lib/libIscore.dylib
+	sudo cp libIscore.1.0.0.dylib /usr/local/lib/
+	sudo ln -s /usr/local/lib/libIscore.1.0.0.dylib /usr/local/lib/libIscore.1.0.dylib
+	sudo ln -s /usr/local/lib/libIscore.1.0.0.dylib /usr/local/lib/libIscore.1.dylib
+	sudo ln -s /usr/local/lib/libIscore.1.0.0.dylib /usr/local/lib/libIscore.dylib
 fi
-sudo ldconfig
 sudo mkdir -p /usr/local/include/IScore
 sudo cp -R Includes/* /usr/local/include/IScore/
 
--- iscore.pro
+++ iscore.pro
@@ -17,6 +17,8 @@ LIBS += -lDeviceManager -lxml2 -lgecodeint -lgecodesearch -lgecodedriver -lgecod
 macx-g++ {
 INCLUDEPATH += . ./Includes /Library/Frameworks/ /usr/local/include/IScore /usr/local/include /usr/local/include/libxml2
 QMAKE_LFLAGS += -L/usr/local/lib/ -L/usr/lib/ -L/System/Library/Frameworks/ -L/Library/Frameworks/
+QMAKE_CFLAGS_X86_64 += -mmacosx-version-min=10.7
+QMAKE_CXXFLAGS_X86_64 = $$QMAKE_CFLAGS_X86_64
 LIBS += -liscore -lDeviceManager -lxml2 -framework gecode
 }
 
