3c82ab67c71b5b6007e458bd4c31cf538bba7aa8
Thomas Meyssonnier <tmeysson@gmail.com>
2013-03-28 13:32:49 +0100

    Removed creation of libIscore.1.0.dylib symlink in install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 if [ -x libIscore.1.0.0.dylib ]
 then
 	sudo cp libIscore.1.0.0.dylib /usr/local/lib/
-	sudo ln -s /usr/local/lib/libIscore.1.0.0.dylib /usr/local/lib/libIscore.1.0.dylib
+#	sudo ln -s /usr/local/lib/libIscore.1.0.0.dylib /usr/local/lib/libIscore.1.0.dylib
 	sudo ln -s /usr/local/lib/libIscore.1.0.0.dylib /usr/local/lib/libIscore.1.dylib
 	sudo ln -s /usr/local/lib/libIscore.1.0.0.dylib /usr/local/lib/libIscore.dylib
 fi
